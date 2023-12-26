8f0acd4fa7afaca8de5ead4b773642e02fd1ea83
Jay Sunkara <73007885+JaySunkara@users.noreply.github.com>
2023-12-26 13:53:03 -0600

    Initial Commit

---
+++ Healthcare Monitor App/Healthcare_Monitor_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Healthcare_Monitor_AppApp.swift
+//  Healthcare Monitor App
+//
+//  Created by Jay Sunkara on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Healthcare_Monitor_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
