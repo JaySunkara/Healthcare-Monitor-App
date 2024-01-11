2e64ee2b665ad1756100a87c578655121cc9ab60
Jay Sunkara <73007885+JaySunkara@users.noreply.github.com>
2024-01-11 15:32:40 -0500

    Added Amplify

--- Healthcare Monitor App/Healthcare_Monitor_AppApp.swift
+++ Healthcare Monitor App/Healthcare_Monitor_AppApp.swift
@@ -4,14 +4,27 @@
 //
 //  Created by Jay Sunkara on 12/26/23.
 //
-
+import Amplify
 import SwiftUI
 
 @main
 struct Healthcare_Monitor_AppApp: App {
+    init() {
+        configureAmplify()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+    private func configureAmplify() {
+        do {
+            try Amplify.configure()
+            print("Successfully configured Amplify")
+        } catch {
+            print("Could not configure Amplify", error)
         }
     }
 }
